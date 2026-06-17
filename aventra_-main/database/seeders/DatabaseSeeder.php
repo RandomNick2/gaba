0c9bc6d946a2862c2e98f2da092e6ac5e4c45b0c
RandomNick2 <mastercode228@gmail.com>
2026-06-17 12:02:39 +0500

    Update DatabaseSeeder.php

--- aventra_-main/database/seeders/DatabaseSeeder.php
+++ aventra_-main/database/seeders/DatabaseSeeder.php
@@ -3,7 +3,9 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,6 +14,16 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        User::updateOrCreate(
+            ['email' => 'admin@qaztour.local'],
+            [
+                'name' => 'Admin',
+                'password' => Hash::make('admin123'),
+                'role' => 'admin',
+                'email_verified_at' => now(),
+            ]
+        );
+
         $this->call([
             TourSeeder::class,
         ]);
